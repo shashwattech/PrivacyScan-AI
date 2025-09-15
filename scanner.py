e9559c9c16d0d10d6da811f16f026751cd6fbcc2
shashwattech <55654957+shashwattech@users.noreply.github.com>
2025-09-16 02:24:00 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -6,8 +6,8 @@ import re
 import json
 
 # OpenAI API configurations
-API_KEY = "9d5bcffba65648fcafc63d8a95a06c83"  # Replace with your actual API key
-ENDPOINT = "https://sdk-team-opnai-eus-poc.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2023-03-15-preview" # replace with the actual endpoint
+API_KEY = "your-llm-api-key"  # Replace with your actual API key
+ENDPOINT = "https://shashwat-team-opnai-eus-poc.openai.azure.com/openai/deployments/gpt-4o/chat/get-your-own-endpoint" # replace with the actual endpoint
 
 def is_not_code_file(filename):
     _, file_extension = os.path.splitext(filename)
@@ -137,3 +137,4 @@ if __name__ == "__main__":
 
     folder_path = sys.argv[1]
     scan_folder_for_pii(folder_path)
+
